9d0f3106d05afb271abf874f2d08d4fbba0f5c41
xVanilla02 <dobry2002@gmail.com>
2024-10-04 11:24:54 +0200

    Randomizer
    randomizer

--- Assets/Scripts/ObjectScanner.cs
+++ Assets/Scripts/ObjectScanner.cs
@@ -5,18 +5,53 @@ using UnityEngine.InputSystem;
 
 public class ObjectScanner : MonoBehaviour
 {
+
+    [System.Serializable]
+    public class AnomalyPair
+    {
+        public GameObject anomalyObject;
+        public GameObject normalObject;
+    }
+    // Anomaly Pair List
+    public List<AnomalyPair> anomalyPairs;
+    // Spawn Location List
+    public List<Transform> spawnLocations;
+    // tag 
     public string Tag = "Anomaly";
     // raycast length
     public float rayLength = 20f;
     // layer mask to scan only scannable objects
     public LayerMask layerMask;
     // right controller position and rotation
-    public Transform rightController; 
+    public Transform rightController;
+
+
+    private void Start()
+    {
+        RandomizeAnomalies();
+    }
+
     void Update()
     {
         if (Keyboard.current.spaceKey.isPressed) // Change the trigger on grip instead
         {
             ShootRay();
+        }
+    }
+
+    void RandomizeAnomalies()
+    {
+        foreach ( var pair in anomalyPairs)
+        {
+            Transform randomLocation = spawnLocations[Random.Range(0, spawnLocations.Count)];
+            pair.anomalyObject.transform.position = randomLocation.position;
+            pair.anomalyObject.transform.rotation = randomLocation.rotation;
+
+            //Enable and disable counterparts
+
+            pair.anomalyObject.SetActive(true);
+            pair.normalObject.SetActive(false);
+
         }
     }
 
@@ -28,9 +63,24 @@ public class ObjectScanner : MonoBehaviour
         // Perform the raycast
         if (Physics.Raycast(ray, out hit, rayLength, layerMask))
         {
-            if (hit.collider.CompareTag(Tag))
+            foreach (var pair in anomalyPairs)
             {
-                Debug.Log("Anomaly Scanned: " + Tag);
+                if (hit.collider.CompareTag(Tag))
+                {
+                    if (hit.collider.gameObject == pair.anomalyObject)
+                    {
+                        Debug.Log("Scanned anomaly" + pair.anomalyObject.name);
+
+                        //replace with counterpart 
+                        pair.anomalyObject.SetActive(false);
+                        pair.normalObject.transform.position = pair.anomalyObject.transform.position;
+                        pair.normalObject.transform.rotation = pair.anomalyObject.transform.rotation;
+                        pair.normalObject.SetActive(true);
+
+                        break; //exit loop after scanning 
+
+                    }
+                }
             }
         }
     }
