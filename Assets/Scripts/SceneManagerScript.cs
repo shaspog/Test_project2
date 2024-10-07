9e0570aef78788f49e21c430b1aaf90baf68ff05
xVanilla02 <dobry2002@gmail.com>
2024-10-07 20:18:11 +0200

    Minor Cleanup

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    //Load Scene
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    //Quit Game
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
     }
 }
