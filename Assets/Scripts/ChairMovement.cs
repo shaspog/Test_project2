5f96bf2c6b6a9bdc6edc48fce31db40e798c8292
xVanilla02 <dobry2002@gmail.com>
2024-10-23 19:36:53 +0200

    chair movement - update
    chair resets position. glides to target location, 25% chance to trigger, chance triggers every 30 seconds

--- Assets/Scripts/ChairMovement.cs
+++ Assets/Scripts/ChairMovement.cs
@@ -7,12 +7,16 @@ public class ChairMovement : MonoBehaviour
     private Vector3 startPosition;  // initial position of the chair
     private bool shouldMove = false; // flag to trigger the movement
 
+    //Sultan
+    public string requireTag = "Anomaly"; //tag 
+    private bool isResetting = false; //reset transform 
+
     void Start()
     {
         startPosition = transform.position; // store the initial position of the chair
 
-        // automatically start moving after 5 seconds
-        Invoke("StartMoving", 5f);
+        // automatically start moving after 5 seconds - Updated Sultan InvokeRepeating calls every 30 second after the first 5 to move the object
+        InvokeRepeating("ChanceStartMoving", 5f, 30f);
     }
  
 
@@ -20,26 +24,64 @@ public class ChairMovement : MonoBehaviour
     {
         if (shouldMove)
         {
+            //Sultan
+            Vector3 destination = isResetting ? startPosition : targetPosition;
+
             // move the chair towards the target position smoothly
-            transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
 
             // if the chair is very close to the target position, stop moving
-            if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
             {
-                shouldMove = false; // Stop the movement
+                //Sultan
+              if (!isResetting)
+                {
+                    shouldMove = false;
+                    Invoke("ReturnToOrigin", 2f);
+                }
+              else
+                {
+                    shouldMove = false;
+                    isResetting = false;
+                }
             }
         }
     }
 
     // triggers the chair to start moving
+    public void ChanceStartMoving()
+    {
+        //Sultan
+        if (gameObject.CompareTag(requireTag))
+        {
+            //Probability chance
+            if (Random.value <= 0.25f)
+            {
+                StartMoving();
+            }
+        }
+    }
+
+    //Sultan
     public void StartMoving()
     {
         shouldMove = true;
     }
 
+    //Sultan
+    public void ReturnToOrigin()
+    {
+        isResetting = true;
+        shouldMove = true;
+    }
+
+
     // can reset the chair to its original position
     public void ResetPosition()
     {
         transform.position = startPosition;
+        //sultan
+        shouldMove = false;
+        isResetting = false;
     }
 }
