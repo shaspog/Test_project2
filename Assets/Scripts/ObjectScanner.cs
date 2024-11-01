75170fc9783fedf1c3a4d294b1d4a6ede9760a99
xVanilla02 <dobry2002@gmail.com>
2024-11-01 21:22:08 +0100

    Anomaly Limit based on the sanity bar and the difficulty setting
    Updated shootRay function, updated difficulty logic, updated the sanity timer logic, added limit to anomalies present, added shuffle to the spawn rate,

--- Assets/Scripts/ObjectScanner.cs
+++ Assets/Scripts/ObjectScanner.cs
@@ -17,8 +17,17 @@ public class ObjectScanner : MonoBehaviour
 
     }
 
+    [System.Serializable]
+    public class DifficultyLevel
+    {
+        public List<AnomalyPair> anomalyPairs; //list for new pairs
+        public int maxActiveAnomalies; //max allowed
+    }
+
     // Anomaly Pair List
     public List<AnomalyPair> anomalyPairs;
+    // Difficulty Level List
+    public List<DifficultyLevel> difficultyLevels;
     // Spawn Location List
     public List<Transform> spawnLocations;
     // tag 
@@ -32,12 +41,13 @@ public class ObjectScanner : MonoBehaviour
     //Input reference for trigger
     public InputActionProperty rightHandTriggerAction; // Works like a charm :3
     //second anomaly delay marker
-    public float secondAnomalyMarker = 0;
+   // public float secondAnomalyMarker = 0;
 
 
     // Difficulty Elements 
     public float difficultyIncreaseSpeed = 40f;
     private float difficultyTimer = 0f;
+    private int currentDifficultyIndex = 0;
 
 
     //UI Elements
@@ -60,7 +70,10 @@ public class ObjectScanner : MonoBehaviour
 
     private void Start()
     {
-        RandomizeAnomalies();
+        if (difficultyLevels.Count > 0)
+        {
+            RandomizeAnomalies(difficultyLevels[0].anomalyPairs);
+        }
         //Timer parts
         currentTime = maxTime;
         anomalyTimerSlider.maxValue = maxTime;
@@ -70,9 +83,12 @@ public class ObjectScanner : MonoBehaviour
         //Difficulty timer 
         difficultyTimer = 0f;
 
+        //Spawn random anomalies
+        StartCoroutine(SpawnAnomalies());
+
         //Next anomaly delay
-        DelayAnomaly(10);
-        secondAnomalyMarker = 1;
+      //  DelayAnomaly(10);
+       // secondAnomalyMarker = 1;
         
 
         // successfulScan = GetComponent<AudioSource>();
@@ -94,11 +110,12 @@ public class ObjectScanner : MonoBehaviour
             }
         }
 
-        if(secondAnomalyMarker == 1)
-        {
-            DelayAnomaly(10);
-            RandomizeAnomalies();
-        }
+        //if(secondAnomalyMarker == 1)
+        //{
+           // DelayAnomaly(10);
+         //   RandomizeAnomalies();
+       // }
+
         //Update Timer Function
         UpdateTimer();
 
@@ -118,7 +135,17 @@ public class ObjectScanner : MonoBehaviour
 
     void UpdateTimer()
     {
+        
+
         int activeAnomalies = CurrentlyActiveAnomalies();
+
+        int anomaliesLimit = 3;
+        int timeInSeconds = Mathf.FloorToInt(currentTime);
+        int addedAnomalies = (timeInSeconds / 10) * 2;
+        anomaliesLimit += addedAnomalies;
+
+        int maxAnomaliesAllowed = Mathf.Min(anomaliesLimit, difficultyLevels[currentDifficultyIndex].maxActiveAnomalies);
+
 
         if (activeAnomalies > 0)
         {
@@ -144,6 +171,17 @@ public class ObjectScanner : MonoBehaviour
             }
         }
 
+        if (currentDifficultyIndex < difficultyLevels.Count)
+        {
+            foreach (var pair in difficultyLevels[currentDifficultyIndex].anomalyPairs)
+            {
+                if (pair.anomalyObject.activeSelf)
+                {
+                    count++;
+                }
+            }
+        }
+
         return count; //return count as total active anomalies
     }
 
@@ -151,14 +189,23 @@ public class ObjectScanner : MonoBehaviour
 
     #region RNG Anomaly
 
-    void RandomizeAnomalies()
+    void RandomizeAnomalies(List<AnomalyPair> pairs)
     {
+        if (pairs.Count == 0 || spawnLocations.Count == 0) return; //making sure list stays full!!
+
+        int maxSpawnCount = Mathf.Min(difficultyLevels[currentDifficultyIndex].maxActiveAnomalies, pairs.Count);
+
         //truly rng anomaly placement
         List<Transform> availableLocations = new List<Transform>(spawnLocations);
+      
 
-        foreach ( var pair in anomalyPairs)
+
+        ShuffleList(pairs);
+
+        for (int I = 0; I < maxSpawnCount; I++)
         {
-            Transform spawnLocation = pair.specificSpawnLocation != null ? pair.specificSpawnLocation : GetRandomSpawnLocation(ref availableLocations); //new addition
+            AnomalyPair pair = pairs[I];
+            Transform spawnLocation = pair.specificSpawnLocation != null ? pair.specificSpawnLocation : GetRandomSpawnLocation(ref availableLocations);
 
             pair.anomalyObject.transform.position = spawnLocation.position;
             pair.anomalyObject.transform.rotation = spawnLocation.rotation;
@@ -168,6 +215,22 @@ public class ObjectScanner : MonoBehaviour
             pair.anomalyObject.SetActive(true);
             pair.normalObject.SetActive(false);
 
+        }
+
+        if (maxSpawnCount >= pairs.Count)
+        {
+            ShuffleList(pairs);
+        }
+    }
+
+    private void ShuffleList(List<AnomalyPair> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            AnomalyPair temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
         }
     }
 
@@ -191,6 +254,26 @@ public class ObjectScanner : MonoBehaviour
         StartCoroutine(AnomalyDelayTime(delayTime));
     }
 
+    IEnumerator SpawnAnomalies()
+    {
+        while (true) //indefinete loop
+        {
+            RandomizeAnomalies(anomalyPairs);
+            yield return new WaitForSeconds(2f);
+
+            if (currentDifficultyIndex < difficultyLevels.Count)
+            {
+                // Spawn Difficult Anomalies!
+                RandomizeAnomalies(difficultyLevels[currentDifficultyIndex].anomalyPairs);
+                yield return new WaitForSeconds(40f);
+            }
+            else
+            {
+                currentDifficultyIndex = 0; // reset to cycle 
+            }
+        }
+    }
+
     #endregion
 
     #region Difficulty
@@ -200,10 +283,21 @@ public class ObjectScanner : MonoBehaviour
 
         if (difficultyTimer >= difficultyIncreaseSpeed)
         {
+            IncreaseDifficulty();
             Debug.Log("Difficulty UPPed! Watch Out Gamers");
             difficultyTimer = 0f; //resets the timer and starts counting down again till next difficulty
         }
     }
+
+    void IncreaseDifficulty()
+    {
+        if (currentDifficultyIndex < difficultyLevels.Count - 1)
+        {
+            currentDifficultyIndex++;
+            RandomizeAnomalies(difficultyLevels[currentDifficultyIndex].anomalyPairs);
+        }
+    }
+
     #endregion
 
     void ShootRay()
@@ -243,28 +337,22 @@ public class ObjectScanner : MonoBehaviour
         {
             if (hitObject.CompareTag(Tag) && hitObject == pair.anomalyObject)
             {
-                Debug.Log("Scanned anomaly" + pair.anomalyObject.name);
-
-                 /*play successful scan audio
-                successfulScan.Play();
-                 */
-
-                //replace with non anomaly counterpart 
-                pair.anomalyObject.SetActive(false);
-                pair.normalObject.transform.position = pair.anomalyObject.transform.position;
-                pair.normalObject.transform.rotation = pair.anomalyObject.transform.rotation;
-                pair.normalObject.SetActive(true);
-
-                // Refill anomaly bar by adding extra time for each scanned anomaly
-                currentTime = Mathf.Min(currentTime + 5f, maxTime);
-                anomalyTimerSlider.value = currentTime;
-
+                ProcAnomalyScan(pair);
                 foundAnomalyTag = true; //AnomalyTag scanned
-
-
-
                 break; //exit loop after scanning 
+            }
+        }
 
+        if (!foundAnomalyTag && currentDifficultyIndex < difficultyLevels.Count)
+        {
+            foreach (var pair in difficultyLevels[currentDifficultyIndex].anomalyPairs)
+            {
+                if (hitObject.CompareTag(Tag) && hitObject == pair.anomalyObject)
+                {
+                    ProcAnomalyScan(pair);
+                    foundAnomalyTag = true;
+                    break;
+                }
             }
         }
 
@@ -277,6 +365,25 @@ public class ObjectScanner : MonoBehaviour
             NonAnomalyScanPunish();
             Debug.Log("Non anomaly scanned" + hitObject.name);
         }
+    }
+
+    void ProcAnomalyScan(AnomalyPair pair)
+    {
+        Debug.Log("Scanned anomaly" + pair.anomalyObject.name);
+
+        /*play successful scan audio
+       successfulScan.Play();
+        */
+
+        //replace with non anomaly counterpart 
+        pair.anomalyObject.SetActive(false);
+        pair.normalObject.transform.position = pair.anomalyObject.transform.position;
+        pair.normalObject.transform.rotation = pair.anomalyObject.transform.rotation;
+        pair.normalObject.SetActive(true);
+
+        // Refill anomaly bar by adding extra time for each scanned anomaly
+        currentTime = Mathf.Min(currentTime + 5f, maxTime);
+        anomalyTimerSlider.value = currentTime;
     }
 
     //Punishment for scanning wrong object
